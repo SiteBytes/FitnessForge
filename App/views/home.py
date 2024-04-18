ccd53c0ae2530a6657cecc79341dbdedd9a056d0
Akash Ramlogan <123250547+dquikbrwnfox@users.noreply.github.com>
2024-04-18 20:01:56 +0000

    Redirect to homepage if user already has jwt

--- App/views/home.py
+++ App/views/home.py
@@ -12,5 +12,5 @@ home_views = Blueprint('home_views', __name__, template_folder='../templates')
 
 @home_views.route('/home', methods=['GET'])
 @jwt_required()
-def home():
+def home_page():
     return render_template('home.html')
