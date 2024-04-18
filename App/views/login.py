63cbaef3f5cf0c155d134ef81f5d3db02cf37f38
Akash Ramlogan <123250547+dquikbrwnfox@users.noreply.github.com>
2024-04-18 20:18:28 +0000

    Modified layout, working on login action

--- App/views/login.py
+++ App/views/login.py
@@ -4,7 +4,6 @@ from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 login_views = Blueprint('login_views', __name__, template_folder='../templates')
 
 @login_views.route('/login', methods=['GET'])
-@jwt_required()
 def login_page():
     if not jwt_current_user:
         return render_template('login.html')
