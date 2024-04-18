c1cb5b8402b6111d771167024c651e5d7ff46d54
Akash Ramlogan <123250547+dquikbrwnfox@users.noreply.github.com>
2024-04-18 19:30:46 +0000

    Updated blueprints, /login

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -3,7 +3,10 @@
 from .user import user_views
 from .index import index_views
 from .auth import auth_views
+from .home import home_views
+from .login import login_views
 
 
-views = [user_views, index_views, auth_views] 
+
+views = [user_views, index_views, auth_views, home_views, login_views] 
 # blueprints must be added to this list
