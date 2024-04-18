dad0307b2337ff2d3c96f8535f35cb2a7ec14b3c
Akash Ramlogan <123250547+dquikbrwnfox@users.noreply.github.com>
2024-04-18 20:01:56 +0000

    Redirect to homepage if user already has jwt

--- App/views/login.py
+++ App/views/login.py
@@ -4,5 +4,9 @@ from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 login_views = Blueprint('login_views', __name__, template_folder='../templates')
 
 @login_views.route('/login', methods=['GET'])
+@jwt_required()
 def login_page():
-    return render_template('login.html')
+    if not jwt_current_user:
+        return render_template('login.html')
+    else:
+        return redirect(url_for('home_views.home_page'))
